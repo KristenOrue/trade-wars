6f59d3ef1ba6b4087b5647130c698be0e9a97a0e
Kira Corbett <kiracorbett00@gmail.com>
2019-11-18 21:23:21 -0800

    Added navigation via Go server

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,9 +6,14 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func welcome(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
+		return
+	}
+
+	if r.Method != http.MethodGet && r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
 
@@ -17,9 +22,27 @@ func home(w http.ResponseWriter, r *http.Request) {
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
+		"./ui/html/index.html",
+		"./ui/html/navigation.html",
 	}
 
+	//ts, err := template.ParseFiles("./ui/html/index.html")
 	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func navigation(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./ui/html/navigation.html")
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -35,8 +58,4 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func players(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Showing our players..."))
-}
-
-func navigation(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Showing our navigation..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,16 +7,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/players", players)
+	mux.HandleFunc("/", welcome)
 	mux.HandleFunc("/navigation", navigation)
+	mux.HandleFunc("/players", players)
 
-	// Create a file server which serves files out of the "./ui/static" directory.
+	// Learn GO: Create a file server which serves files out of the "./ui/static" directory.
 	// Note that the path given to the http.Dir function is relative to the project
 	// directory root.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	// Use the mux.Handle() function to register the file server as the handler for
+	// Learn GO: Use the mux.Handle() function to register the file server as the handler for
 	// all URL paths that start with "/static/". For matching paths, we strip the
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
