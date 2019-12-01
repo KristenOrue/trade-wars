42c068cbf91a21be6f709c175a521f3c242219be
Kristen Orue <korue113@gmail.com>
2019-12-01 14:22:55 -0800

    Add single image for ship

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,3 +1,5 @@
+const BASE_URL = "https://stranger-trade-wars.herokuapp.com"
+
 var navLinks = document.querySelectorAll("nav a");
 for (var i = 0; i < navLinks.length; i++) {
 	var link = navLinks[i]
@@ -6,3 +8,19 @@ for (var i = 0; i < navLinks.length; i++) {
 		break;
 	}
 }
+
+var grid = document.getElementsByClassName("grid");
+var ctx = grid.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+
+
+let elem = document.elementFromPoint(x, y);
+let centerX = document.documentElement.clientWidth / 2;
+let centerY = document.documentElement.clientHeight / 2;
+
+let elem = document.elementFromPoint(centerX, centerY);
+
+elem.style.background = "red";
+alert(elem.tagName);
